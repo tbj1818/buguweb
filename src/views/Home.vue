4008987a7fa5657ebf9c508d442c1e92be38d509
tbj <tangbaojie@126.com>
2019-05-07 11:04:52 +0800

    demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,31 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="van-ellipsis">这是一段宽度限制 250px 的文字，后面的内容会省略</div>
+    <!-- 淡入 -->
+<transition name="van-fade">
+  <div v-show="visible">Fade</div>
+</transition>
+
+<!-- 上滑进入 -->
+<transition name="van-slide-up">
+  <div v-show="visible">Slide Up</div>
+</transition>
+
+<!-- 下滑进入 -->
+<transition name="van-slide-down">
+  <div v-show="visible">Slide Down</div>
+</transition>
+
+<!-- 左滑进入 -->
+<transition name="van-slide-left">
+  <div v-show="visible">Slide Left</div>
+</transition>
+
+<!-- 右滑进入 -->
+<transition name="van-slide-right">
+  <div v-show="visible">Slide Right</div>
+</transition>
   </div>
 </template>
 
